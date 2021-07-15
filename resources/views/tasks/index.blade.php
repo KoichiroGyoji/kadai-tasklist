b1de33b48b7df3f346886b16d1da0a289070b9c1
koichiro.gyoji <toko.gyoji.1210@gmail.com>
2021-07-15 08:56:53 +0000

    change name

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -15,7 +15,7 @@
             <tbody>
                 @foreach ($tasks as $task)
                 <tr>
-                    {{-- メッセージ詳細ページへのリンク --}}
+                    {{-- タスク詳細ページへのリンク --}}
                     <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
                     <td>{{ $task->content }}</td>
                 </tr>
